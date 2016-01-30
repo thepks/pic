42733c32abe0129a103ef11b0293258f1db0c568
Graham Parkes <graham@theparkes.me.uk>
2016-01-30 13:44:28 +0000

    Added counter to LCD test

--- TestLcd.asm
+++ TestLcd.asm
@@ -4,6 +4,8 @@
 	__CONFIG h'3f58' 
 
 	ORG 0x20
+speed_enable  EQU 0x71
+speed0 EQU 0x72
 speed1 EQU 0x73
 Save_W EQU 0x75
 Save_Status EQU 0x76
@@ -29,11 +31,44 @@ Reg_Select  EQU 0x79                ;LCD register select flag
         MOVWF   Save_W
         SWAPF   STATUS, W
         MOVWF   Save_Status
-	MOVF speed1,w
 ; now the processing
-	INCF speed1,1
-doneit: NOP
+	MOVF speed_enable, w
+	BTFSC STATUS,Z
+	GOTO end_fie
+	INCF speed1,f
+	MOVF speed1,w
+	BCF STATUS,Z
+	ANDLW 0xF0
+	BTFSS STATUS,Z
+	GOTO display_speed0 
+	GOTO end_fie
+display_speed0: NOP	
+	MOVLW   Cmnd_Mode
+        MOVWF   Reg_Select
+        MOVLW   0x80            ;write at start of line 1 (address 0x00)
+        CALL    Send_Byte
+        CALL    Delay50us
+        MOVLW   Data_Mode
+        MOVWF   Reg_Select
+
+	MOVF speed0,W
+	ANDLW 0xF0
+	SWAPF W,W
+	CALL Number_lookup
+	CALL Send_Byte
+
+	MOVF speed0,W
+	ANDLW 0x0F
+	CALL Number_lookup
+	CALL Send_Byte
+	MOVLW 0x0
+	MOVWF speed1
+	INCF speed0,F
+
+
+end_fie: NOP
 	BANKSEL INTCON
+
 	BCF INTCON, INTF
 	BCF INTCON, T0IF
 	BANKSEL PORTB
@@ -45,6 +80,8 @@ doneit: NOP
 	RETFIE
 	
 setup:
+	MOVLW 0x00
+	MOVWF speed_enable
 	BANKSEL CMCON
 	MOVLW 0x07					;	This will turn the comparators OFF.
 	MOVWF CMCON
@@ -77,6 +114,7 @@ setup:
 	BANKSEL PORTB
 	MOVLW 0x00
 	MOVWF speed1
+	MOVWF speed0
 
 	;clear output
 	MOVWF PORTB
@@ -85,6 +123,8 @@ setup:
 	CALL Delay100ms
 	CALL Init_LCD
 	CALL Test_Code1
+	MOVLW 0x01
+	MOVWF speed_enable 
 
 
 begin: NOP
@@ -128,6 +168,10 @@ Init_LCD    MOVLW   Cmnd_Mode       ;all init done in LCD command mode
             CALL    Send_Byte
             CALL    Delay50us
 	    CALL    Delay50us
+            MOVLW   0x01
+            CALL    Send_Byte
+            CALL    Delay50us
+	    CALL    Delay50us
             RETURN
 
 Send_Byte  	MOVWF   Data_Byte       ;Send what's in W
@@ -158,11 +202,34 @@ Test_Code1  MOVLW   Cmnd_Mode
             CALL    Delay50us
             MOVLW   Data_Mode
             MOVWF   Reg_Select
-            MOVLW   0x48 
+            MOVLW   'H'
             CALL    Send_Byte
-            MOVLW   0x49
+            MOVLW   'E' 
             CALL    Send_Byte
+	    MOVLW   'L'
+	    CALL    Send_Byte
+	    MOVLW   'L'
+	    CALL    Send_Byte
+	    MOVLW   'O'
+	    CALL    Send_Byte	
             RETURN
 
+Number_lookup	ADDWF PCL,f
+		RETLW '0'
+		RETLW '1'
+		RETLW '2'
+		RETLW '3'
+		RETLW '4'
+		RETLW '5'
+		RETLW '6'
+		RETLW '7'
+		RETLW '8'
+		RETLW '9'
+		RETLW 'A'
+		RETLW 'B'
+		RETLW 'C'
+		RETLW 'D'
+		RETLW 'E'
+		RETLW 'F'
 
    	END	
